122c5d7fa7a185b87e4488db1fb48ff73d71319d
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2017-04-08 10:22:58 +0200

    Added atom_uninstall

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npm install --global npm@latest
-npm install --global yo
-npm install --global bower
-npm install --global generator-aspnet
---
+++ uninstall_atom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm /usr/local/bin/atom
+rm /usr/local/bin/apm
+rm -rf ~/atom
+rm -rf ~/.atom
+rm -rf ~/.config/Atom-Shell
+rm -rf /usr/local/share/atom/
+
